9ddec44f884cb5fbfd825b897857bb441367a479
Dhruv <dhruvkathiriya933@gmail.com>
2023-12-03 00:18:03 +0530

    lec A to Z latter...

---
+++ T.c
@@ -0,0 +1,25 @@
+/* 
+
+***********
+     *
+     *
+     *
+     *
+     *
+     *
+*/
+
+#include<stdio.h>
+
+int main(){
+	
+	int i;
+	
+	for(i=1; i<=7; i++){
+		if(i==1){
+			printf("***********\n");
+		}else{
+			printf("     *\n");
+		}
+	}
+}
